15c35f67c4e35d1e2e38e94d6ddad331ebcf4b86
刘元博 <1553592282@qq.com>
2022-08-06 23:32:47 +0800

    测试

--- src/views/gh-login.vue
+++ src/views/gh-login.vue
@@ -1,25 +1,45 @@
 <template>
-  gh-login
+  <div v-if="user">
+    <button>{{ user.name }}</button>
+    <button @click="logout">退出登录</button>
+  </div>
+  <button v-else @click="login">登录</button>
 </template>
 
 <script setup>
 import { Octokit } from '@octokit/core'
 import { createOAuthUserClientAuth } from 'octokit-auth-oauth-user-client'
+import { ref } from 'vue'
 
 const octokit = new Octokit({
   authStrategy: createOAuthUserClientAuth,
   auth: {
     clientId: 'd18fe7df0f5527d36cd8',
-    serviceOrigin: 'http://127.0.0.1:3000',
-    servicePathPrefix: '/api/github/oauth/callback'
+    serviceOrigin: 'https://oauth-app-seven.vercel.app',
+    servicePathPrefix: '/api/github/oauth'
   }
 })
 
-const auth = await octokit.auth()
-if (!auth) {
-  await octokit.auth({ type: 'signIn' })
-} else {
-  console.log(await octokit.request('GET /user'))
+const user = ref()
+octokit.auth().then(auth => {
+  if (!auth) return
+  octokit.request('GET /user').then(data => {
+    console.log(data.data)
+    user.value = data.data
+  })
+})
+
+function login () {
+  octokit.auth({ type: 'signIn' })
+}
+
+function logout () {
+  octokit.auth({
+    type: 'deleteToken',
+    offline: true
+  }).then(() => {
+    user.value = undefined
+  })
 }
 </script>
 
