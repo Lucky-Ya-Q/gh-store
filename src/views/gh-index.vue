4d0f1859f2f75791ba26fcddca6aab8e1d6a34e8
刘元博 <1553592282@qq.com>
2022-08-09 10:47:21 +0800

    修bug

--- src/views/gh-index.vue
+++ src/views/gh-index.vue
@@ -1,5 +1,26 @@
 <template>
-  gh-index
+  <n-h3>gh-store 是一款基于 GitHub API & jsDelivr 开发的具有 CDN 加速功能的图床工具<br>
+    无需下载！无需安装！打开网站即用！
+  </n-h3>
+  <n-h3>本站前后端全部部署在 Vercel 所以敢保证！免费！稳定！</n-h3>
+  <p>建议将本站添加至浏览器收藏夹，方便下次使用 😊</p>
+  <p> 作者：
+    <n-button text tag="a" href="https://github.com/Lucky-Ya-Q" target="_blank" type="primary">
+      也曾为你像超人
+    </n-button>
+    GitHub：
+    <n-button text tag="a" href="https://github.com/Lucky-Ya-Q/gh-store" target="_blank" type="primary">
+      gh-store
+    </n-button>
+  </p>
+  <p>在使用过程中遇到问题，请给作者提 Issue</p>
+  <n-h3 style="color:red">图片上传缓慢或加载不出来等情况，可借助
+    <n-button text tag="a" href="https://github.com/Alvin9999/new-pac/wiki" target="_blank" type="primary">
+      科学上网
+    </n-button>
+    工具
+  </n-h3>
+  <n-h3>郑重声明：请勿通过本站上传违反你当地法律的图片，所造成的一切后果与本站无关</n-h3>
 </template>
 
 <script setup>
@@ -7,5 +28,7 @@
 </script>
 
 <style scoped lang="scss">
-
+p {
+  font-size: 16px;
+}
 </style>
